2b0456061ed65d1474ce5804e41492e319d58554
Wojciech Pragłowski <44020049+Praglu@users.noreply.github.com>
2021-09-19 17:45:17 +0200

    Update settings.py

--- tracker/settings.py
+++ tracker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY =
+# SECRET_KEY = 'django-insecure-w7b3pfoy(=iwa9w7dizc8c(vyne*29!b!^e9w$m-^n+sz-26f5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
